fb41d1809d67c3b2528fad291da92cf351e26b3b
Mustakim (Rafid) <mustakimrafid126@gmail.com>
2025-06-22 23:08:50 +0600

    update vercel.json

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import dotenv from "dotenv"
 import { dbConnect } from "./app/db";
 import { app } from "./app";
+import { Request, Response } from "express";
 
-const port = 5000
+const port = 3000
 
 dotenv.config({
     path: "./.env"
@@ -10,6 +11,9 @@ dotenv.config({
 
 dbConnect()
     .then(() => {
+        app.get("/", (req: Request, res: Response) => {
+            res.send("Library management system")
+        });
         app.listen(port, () => {
             console.log(`⚙️  Example app listening on port ${port}`)
         })
