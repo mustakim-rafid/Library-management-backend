a65fb1e24636c477d389dc7db1df0af61c2e5493
Mustakim (Rafid) <mustakimrafid126@gmail.com>
2025-07-03 13:50:48 +0600

    add cors policy

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import express, { Request, Response } from "express"
+import cors from "cors"
 
 const app = express()
+
+app.use(
+    cors({
+        origin: [
+            'http://localhost:5173'
+        ]
+    })
+)
 
 app.use(express.json())
 
