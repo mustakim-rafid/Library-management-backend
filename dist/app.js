fa39f8ce1114df78e82c02e458c97260090e51a3
Mustakim (Rafid) <mustakimrafid126@gmail.com>
2025-07-03 17:40:17 +0600

    cors fix

--- dist/app.js
+++ dist/app.js
@@ -5,8 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 exports.app = app;
+app.use((0, cors_1.default)({
+    origin: [
+        'http://localhost:5173'
+    ]
+}));
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.json({
